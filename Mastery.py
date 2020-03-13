635ab61d1a1f107ed9172ff5f85ea2b530354f37
23nichje <59451278+23nichje@users.noreply.github.com>
2020-03-13 10:27:56 -0400

    Add files via upload

---
+++ Mastery.py
@@ -0,0 +1,21 @@
+         
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+student_Id = input("What is your student ID? ")
+
+class_one = input("Enter the next class?, Stop to end ")
+roomnum1 = int(input("Enter the room number? "))
+class_two = input("Enter the next class?, Stop to end ")
+roomnum2 = int(input("Enter the room number? "))
+class_three = input("Enter the next class?, Stop to end ")
+roomnum3 = int(input("Enter the room number? "))
+class_four = input("Enter the next class?, Stop to end ")
+roomnum4 = int(input("Enter the room number? "))
+class_five = input("Enter the next class?, Stop to end ")
+roomnum5 = int(input("Enter the room number? "))
+class_six = input("Enter the next class?, Stop to end ")
+roomnum6 = int(input("Enter the room number? "))
+
+ 
+
+print(f"*************************************************\n*\t\t\t\t\t\t*\n*\t{last_name}, {first_name}\tID: {student_Id}\t*\n*\t\t\t\t\t\t*\n*************************************************\n*\t\t\t\t\t\t*\n*\tBlock 1: {class_one}\t Room: {roomnum1}\t*\n*\tBlock 2: {class_two}\t Room: {roomnum2}\t*\n*\tBlock 2: {class_three}\t Room: {roomnum3}\t*\n*\tBlock 4: {class_four}\t Room: {roomnum4}\t*\n*\tBlock 5: {class_five}\t Room: {roomnum5}\t*\n*\t Room: {class_six}\t Room: {roomnum6}\t*\n*                                               *\n*                                               *\n*\t\t\t\t\t\t*\n*************************************************\n*                                               *\n*************************************************") 
